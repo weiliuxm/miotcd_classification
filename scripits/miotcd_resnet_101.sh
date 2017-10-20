8e78d27ac01c40aa3ada72523e50a9291952a731
Wei Liu <284613412@qq.com>
2017-10-20 14:34:56 +0800

    Update and rename miotcd_resnet_50.sh to miotcd_resnet_101.sh

--- scripits/miotcd_resnet_101.sh
+++ scripits/miotcd_resnet_101.sh
@@ -1,15 +1,15 @@
 
 DATASET_DIR=/home/weiliu/data/datasets/mio-tcd/tfrecord/miotcd_classification_tfrecord
-TRAIN_DIR=/media/weiliu/data/results/miotcd/classification/models/resnet_v2_101_ls
-DATASET_DIR=/media/weiliu/data/datasets/mio-tcd/tfrecord/miotcd_classification_tfrecord_test
+TRAIN_DIR=/home/weiliu/data/results/miotcd/classification/models/resnet_v2_101_ls
+DATASET_DIR=/home/weiliu/data/datasets/mio-tcd/tfrecord/miotcd_classification_tfrecord_test
 EVAL_DIR= /home/weiliu/data/results/miotcd/classification/evaluation/resnet_v2_101_ls
 CHECKPOINT_PATH=/home/weiliu/data/models/tensorflow/slim/resnet_v2_101_2017_04_14/resnet_v2_101.ckpt
 NUM_CLASSES=11
 NUM_SAMPLES=519164
 WEIGHT_FLAG=True
 MODEL_NAME=resnet_v2_101
-LABELS_TO_NAMES_PATH=../datasets/miotcd_labels.txt
-python ../train_image_classifier_miotcd.py \
+LABELS_TO_NAMES_PATH=datasets/miotcd_labels.txt
+python train_image_classifier_miotcd.py \
     --train_dir=${TRAIN_DIR} \
     --dataset_dir=${DATASET_DIR} \
     --num_classes=${NUM_CLASSES} \
@@ -19,7 +19,7 @@ python ../train_image_classifier_miotcd.py \
     --label_smoothing=0.1 \
     --end_learning_rate=0.00001 \
     --dataset_split_name=train \
-    --batch_size=128 \
+    --batch_size=64 \
     --model_name=${MODEL_NAME} \
     --checkpoint_path=${CHECKPOINT_PATH} \
     --max_number_of_steps=80000 \
@@ -27,7 +27,7 @@ python ../train_image_classifier_miotcd.py \
 ##    --weights_flag=${WEIGHTS_FLAG} \
 
 
-python ../eval_image_classifier_miotcd.py \
+python eval_image_classifier_miotcd.py \
     --checkpoint_path=${TRAIN_DIR} \
     --eval_dir=${EVAL_DIR} \
     --dataset_split_name=validation \
