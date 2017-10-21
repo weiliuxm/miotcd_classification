b81eb1f4b1a87c35752358690a7a02dc3d21a767
Wei Liu <284613412@qq.com>
2017-10-21 21:01:01 +0800

    Update convert_data.sh

--- scripits/convert_data.sh
+++ scripits/convert_data.sh
@@ -1,1 +1,1 @@
-
+python convert_data.py --dataset_dir=/media/Store/weiliu/datasets/MIO-TCD/MIO-TCD-Classification --dataset_dir_conv=/media/Store/weiliu/datasets/MIO-TCD/miotcd_classification_tfrecord_test
