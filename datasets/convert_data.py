b6adca42976e31b625a653db95e6051cc62be379
Wei Liu <284613412@qq.com>
2017-10-23 09:54:16 +0800

    Update convert_data.py

--- datasets/convert_data.py
+++ datasets/convert_data.py
@@ -124,7 +124,7 @@ def _convert_dataset(split_name, filenames, class_names_to_ids, dataset_dir):
             class_id = class_names_to_ids[class_name]
 
             example = dataset_utils.image_to_tfexample(
-                image_data, b'jpg', height, width, class_id)
+                image_data, b'jpg', height, width, class_id, filenames[i])
             tfrecord_writer.write(example.SerializeToString())
 
   sys.stdout.write('\n')
