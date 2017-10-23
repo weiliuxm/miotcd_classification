5166c082ca497598f844e2f47f1a8f0779ebde0b
Wei Liu <284613412@qq.com>
2017-10-23 11:13:20 +0800

    Update and rename convert_data.py to convert_data_test.py

--- datasets/convert_data_test.py
+++ datasets/convert_data_test.py
@@ -124,8 +124,8 @@ def _convert_dataset(split_name, filenames, class_names_to_ids, dataset_dir):
             class_name = os.path.basename(os.path.dirname(filenames[i]))
             class_id = class_names_to_ids[class_name]
 
-            example = dataset_utils.image_to_tfexample(
-                image_data, b'jpg', height, width, class_id, filenames[i])
+            example = dataset_utils.image_to_tfexample_test(
+                image_data, 'jpg', height, width, class_id, filenames[i])
             tfrecord_writer.write(example.SerializeToString())
 
   sys.stdout.write('\n')
