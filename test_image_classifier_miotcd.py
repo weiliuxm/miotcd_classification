497bd257e3d515a6c328170283f662b1b07fb1d7
Wei Liu <284613412@qq.com>
2017-11-10 22:13:24 +0800

    Update test_image_classifier_miotcd.py

--- test_image_classifier_miotcd.py
+++ test_image_classifier_miotcd.py
@@ -1,207 +1,134 @@
-# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""creat prediction results with a csv file for MIO-TCD tesing set."""
-# 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
+import os
 import math
+import csv
 import tensorflow as tf
-
-#from datasets import dataset_factory
-from datasets import dataset_classification_test   #dataset_classification_test
 from nets import nets_factory
 from preprocessing import preprocessing_factory
-
 slim = tf.contrib.slim
-
-tf.app.flags.DEFINE_integer(
-    'batch_size', 100, 'The number of samples in each batch.')
-
-tf.app.flags.DEFINE_integer(
-    'max_num_batches', None,
-    'Max number of batches to evaluate by default use all.')
-
 tf.app.flags.DEFINE_string(
     'master', '', 'The address of the TensorFlow master to use.')
-
 tf.app.flags.DEFINE_string(
     'checkpoint_path', '/tmp/tfmodel/',
     'The directory where the model was written to or an absolute path to a '
     'checkpoint file.')
-
 tf.app.flags.DEFINE_string(
-    'eval_dir', '/tmp/tfmodel/', 'Directory where the results are saved to.')
-
-tf.app.flags.DEFINE_integer(
-    'num_preprocessing_threads', 8,
-    'The number of threads used to create the batches.')
-
-tf.app.flags.DEFINE_string(
-    'dataset_name', 'imagenet', 'The name of the dataset to load.')
-
-tf.app.flags.DEFINE_string(
-    'dataset_split_name', 'test', 'The name of the train/test split.')
-
-tf.app.flags.DEFINE_string(
-    'dataset_dir', None, 'The directory where the dataset files are stored.')
-
-
-tf.app.flags.DEFINE_integer(
-    'num_samples', 129795, 'Number of samples.')
+    'test_dir', None, 'Test image directory.')
+tf.app.flags.DEFINE_string('results_dir','','The directory where the prediction results are written')
+tf.app.flags.DEFINE_string('label_path','datasets/labels.txt','The path to the labels.txt')
 tf.app.flags.DEFINE_integer(
     'num_classes', 11, 'Number of classes.')
-tf.app.flags.DEFINE_string(
-    'labels_to_names_path', None, 'Label names file path.')
-
-
-
 tf.app.flags.DEFINE_integer(
     'labels_offset', 0,
     'An offset for the labels in the dataset. This flag is primarily used to '
     'evaluate the VGG and ResNet architectures which do not use a background '
     'class for the ImageNet dataset.')
-
 tf.app.flags.DEFINE_string(
     'model_name', 'inception_v3', 'The name of the architecture to evaluate.')
-
 tf.app.flags.DEFINE_string(
     'preprocessing_name', None, 'The name of the preprocessing to use. If left '
     'as `None`, then the model_name flag is used.')
-
+tf.app.flags.DEFINE_integer(
+    'test_image_size', None, 'Eval image size')
 tf.app.flags.DEFINE_float(
     'moving_average_decay', None,
     'The decay to use for the moving average.'
     'If left as None, then moving averages are not used.')
 
-tf.app.flags.DEFINE_integer(
-    'eval_image_size', None, 'Eval image size')
-
 FLAGS = tf.app.flags.FLAGS
-
-
 def main(_):
-  if not FLAGS.dataset_dir:
-    raise ValueError('You must supply the dataset directory with --dataset_dir')
+#    if not FLAGS.test_list:
+#        raise ValueError('You must supply the test list with --test_list')
 
-  tf.logging.set_verbosity(tf.logging.INFO)
-  with tf.Graph().as_default():
-    tf_global_step = slim.get_or_create_global_step()
+    if not FLAGS.test_dir:
+        raise  ValueError('You must supply the test image directory')
+    image_dir = FLAGS.test_dir
+    tf.logging.set_verbosity(tf.logging.INFO)
 
-    ######################
-    # Select the dataset #
-    ######################
-#    dataset = dataset_factory.get_dataset(
-#        FLAGS.dataset_name, FLAGS.dataset_split_name, FLAGS.dataset_dir)
-    dataset = dataset_classification_test.get_dataset(
-        FLAGS.dataset_dir, FLAGS.num_samples, FLAGS.num_classes, FLAGS.labels_to_names_path)
-    ####################
-    # Select the model #
-    ####################
-    network_fn = nets_factory.get_network_fn(
-        FLAGS.model_name,
-        num_classes=(dataset.num_classes - FLAGS.labels_offset),
-        is_training=False)
+    num_images = len(os.listdir(image_dir))
+    file_names = os.listdir(image_dir)
 
-    ##############################################################
-    # Create a dataset provider that loads data from the dataset #
-    ##############################################################
-    provider = slim.dataset_data_provider.DatasetDataProvider(
-        dataset,
-        shuffle=False,
-        common_queue_capacity=2 * FLAGS.batch_size,
-        common_queue_min=FLAGS.batch_size)
-    [image, label] = provider.get(['image', 'label'])
-    label -= FLAGS.labels_offset
+    file_full_names=[]
+    for image_id in range(num_images):
+        file_full_name = os.path.join(image_dir,file_names[image_id])
+        file_full_names.append(file_full_name)
 
-    #####################################
-    # Select the preprocessing function #
-    #####################################
-    preprocessing_name = FLAGS.preprocessing_name or FLAGS.model_name
-    image_preprocessing_fn = preprocessing_factory.get_preprocessing(
-        preprocessing_name,
-        is_training=False)
 
-    eval_image_size = FLAGS.eval_image_size or network_fn.default_image_size
+    labels_fd=open(FLAGS.label_path,"r")
+    class_names = [row.strip().split(':')[1] for row in labels_fd]
+    print(class_names)
 
-    image = image_preprocessing_fn(image, eval_image_size, eval_image_size)
+    csv_results_file_fullname =os.path.join(FLAGS.results_dir, FLAGS.model_name+"_results_test.csv")
+    print(FLAGS.results_dir)
+    print(csv_results_file_fullname)
 
-#    images, labels = tf.train.batch(
-#        [image, label],
-#        batch_size=FLAGS.batch_size,
-#        num_threads=FLAGS.num_preprocessing_threads,
-#        capacity=5 * FLAGS.batch_size)
-    images, labels, filenames = tf.train.batch(
-        [image, label, filename],
-        batch_size=FLAGS.batch_size,
-        num_threads=FLAGS.num_preprocessing_threads,
-        capacity=5 * FLAGS.batch_size)
-    ####################
-    # Define the model #
-    ####################
-    logits, _ = network_fn(images)
+    csv_out = open(csv_results_file_fullname,'wb')
+    results_csv = csv.writer(csv_out,delimiter='\t')
 
-    if FLAGS.moving_average_decay:
-      variable_averages = tf.train.ExponentialMovingAverage(
-          FLAGS.moving_average_decay, tf_global_step)
-      variables_to_restore = variable_averages.variables_to_restore(
-          slim.get_model_variables())
-      variables_to_restore[tf_global_step.op.name] = tf_global_step
-    else:
-      variables_to_restore = slim.get_variables_to_restore()
+    with tf.Graph().as_default():
+        tf_global_step = slim.get_or_create_global_step()
 
-    predictions = tf.argmax(logits, 1)
-    labels = tf.squeeze(labels)
+        ####################
+        # Select the model #
+        ####################
+        network_fn = nets_factory.get_network_fn(
+            FLAGS.model_name,
+            num_classes=FLAGS.num_classes,
+            is_training=False)
 
-    # Define the metrics:
-    names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
-        'Accuracy': slim.metrics.streaming_accuracy(predictions, labels),
-        'Recall_5': slim.metrics.streaming_recall_at_k(
-            logits, labels, 5),
-    })
+        #####################################
+        # Select the preprocessing function #
+        #####################################
+        preprocessing_name = FLAGS.preprocessing_name or FLAGS.model_name
+        image_preprocessing_fn = preprocessing_factory.get_preprocessing(
+            preprocessing_name,
+            is_training=False)
 
-    # Print the summaries to screen.
-    for name, value in names_to_values.items():
-      summary_name = 'eval/%s' % name
-      op = tf.summary.scalar(summary_name, value, collections=[])
-      op = tf.Print(op, [value], summary_name)
-      tf.add_to_collection(tf.GraphKeys.SUMMARIES, op)
+        test_image_size = FLAGS.test_image_size or network_fn.default_image_size
 
-    # TODO(sguada) use num_epochs=1
-    if FLAGS.max_num_batches:
-      num_batches = FLAGS.max_num_batches
-    else:
-      # This ensures that we make a single pass over all of the data.
-      num_batches = math.ceil(dataset.num_samples / float(FLAGS.batch_size))
+        image_string = tf.placeholder(tf.string)
+        image = tf.image.decode_jpeg(image_string, channels=3,
+                                     try_recover_truncated=True,
+                                     acceptable_fraction=0.3)
 
-    if tf.gfile.IsDirectory(FLAGS.checkpoint_path):
-      checkpoint_path = tf.train.latest_checkpoint(FLAGS.checkpoint_path)
-    else:
-      checkpoint_path = FLAGS.checkpoint_path
+        processed_image = image_preprocessing_fn(image, test_image_size, test_image_size)
+        processed_image = tf.expand_dims(processed_image, 0)
 
-    tf.logging.info('Evaluating %s' % checkpoint_path)
+        ####################
+        # Define the model #
+        ####################
+        logits, _ = network_fn(processed_image)
 
-    slim.evaluation.evaluate_once(
-        master=FLAGS.master,
-        checkpoint_path=checkpoint_path,
-        logdir=FLAGS.eval_dir,
-        num_evals=num_batches,
-        eval_op=list(names_to_updates.values()),
-        variables_to_restore=variables_to_restore)
+        if FLAGS.moving_average_decay:
+            variable_averages = tf.train.ExponentialMovingAverage(
+                FLAGS.moving_average_decay, tf_global_step)
+            variables_to_restore = variable_averages.variables_to_restore(
+                slim.get_model_variables())
+            variables_to_restore[tf_global_step.op.name] = tf_global_step
+        else:
+            variables_to_restore = slim.get_variables_to_restore()
+
+        predictions = tf.argmax(logits, 1)
+
+        if tf.gfile.IsDirectory(FLAGS.checkpoint_path):
+            checkpoint_path = tf.train.latest_checkpoint(FLAGS.checkpoint_path)
+        else:
+            checkpoint_path = FLAGS.checkpoint_path
+
+        init_fn = slim.assign_from_checkpoint_fn(checkpoint_path, variables_to_restore)
+
+        with tf.Session() as sess:
+            init_fn(sess)
+            for file_full_name in file_full_names:
+                x = open(file_full_name).read()
+                pred = sess.run(predictions, feed_dict={image_string: x})
+                print("the predict result of {}.jpg is {}".format(file_full_name.split('/')[-1].split('.')[0],class_names[int(pred[0])]))
+                results_csv.writerow([file_full_name.split('/')[-1].split('.')[0], class_names[int(pred[0])]])
+
+    csv_out.close()
 
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
