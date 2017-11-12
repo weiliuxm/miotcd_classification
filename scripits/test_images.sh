8b88994c0e178b9c84151b68f64c9a11f2b406ad
Wei Liu <284613412@qq.com>
2017-11-12 21:41:27 +0800

    Update test_images.sh

--- scripits/test_images.sh
+++ scripits/test_images.sh
@@ -3,10 +3,13 @@ LABES_TO_NAMES_PATH=/home/weiliu/tools/slim/models/research/slim/datasets/labels
 TEST_DIR=/media/weiliu/data/datasets/mio-tcd/images/test
 RESULTS_DIR=/media/weiliu/data/results/miotcd/classification/csvResults
 MODEL_NAME=resnet_v2_50
+PREFIX_CSV=resnet_v2_50_ls_weighted
 NUM_CLASSES=11
-python test_image_classifier.py \
+python test_image_classifier_miotcd.py \
     --checkpoint_path=${TRAIN_DIR} \
     --test_dir=${TEST_DIR} \
+    --prefix_csv=${PREFIX_CSV}
     --num_classes=11 \
     --results_dir=${RESULTS_DIR} \
     --model_name=${MODEL_NAME}
+
